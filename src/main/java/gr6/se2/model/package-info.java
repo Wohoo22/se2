926f7aaa71b78f42fa2e9214c96792f09e03ceb8
Wohoo22 <quandang2102@gmail.com>
2024-03-06 01:23:56 +0700

    init

---
+++ src/main/java/gr6/se2/model/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the definition of every data models
+ * persisted in MySQL, and their corresponding repositories
+ * <p>
+ * To CRUD data from MySQL, we can use data and functions from this package
+ */
+package gr6.se2.model;
